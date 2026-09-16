7cd62d9e0789fd4e4e56eadfd07d550d5af385f7
Aria Zare <ariaaazare@gmail.com>
2026-09-16 07:15:20 +0200

    [+] end of chater 22

--- BreakAfterRegex.py
+++ BreakAfterRegex.py
@@ -1,7 +1,61 @@
 import lldb
 
 def breakAfterRegex(debugger, command, result, internal_dict):
-    print("yay. basic script setup with input: {}".format(command))
+    target = debugger.GetSelectedTarget()
+    breakpoint = target.BreakpointCreateByRegex(command)
 
-def __break_init_module(debugger, internal_dict):
+    if not breakpoint.IsValid() or breakpoint.num_locations == 0:
+        result.AppendWarning("breakpoint is not valid or has not found any hits.")
+    else:
+        result.AppendMessage("{}".format(breakpoint))
+        
+    breakpoint.SetScriptCallbackFunction("BreakAfterRegex.breakpointHandler")
+
+def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand('command script add -f BreakAfterRegex.breakAfterRegex bar')
+
+def breakpointHandler(frame, bp_loc, dict):
+    '''function called when the regular expression breakpoint gets triggered'''
+
+    thread = frame.GetThread()
+    process = thread.GetProcess()
+    debugger = process.GetTarget().GetDebugger()
+
+    function_name = frame.GetFunctionName()
+
+    debugger.SetAsync(False)
+
+    thread.StepOut()
+
+    output = evaluateReturnedObject(debugger, thread, function_name)
+
+    if output is not None:
+        print(output)
+     
+    # print("stopped in: {}".format(function_name))
+    return True
+
+def evaluateReturnedObject(debugger, thread, function_name):
+    
+    '''
+    Grabs the reference from the return register and returns a string from the evaluated value.
+    TODO ObjC only.
+    '''
+
+    res = lldb.SBCommandReturnObject()
+
+    interpreter = debugger.GetCommandInterpreter()
+    target = debugger.GetSelectedTarget()
+    frame = thread.GetSelectedFrame()
+    parent_function_name = frame.GetFunctionName()
+
+    expression = 'expression -l objc -O -- $arg1'
+
+    interpreter.HandleCommand(expression, res)
+
+    if res.HasResult():
+        output = '{}\nbreakpoint: {}\nobject: {}\nstopped: {}'.format('*' * 80, function_name, res.GetOutput().replace('\n', ''), parent_function_name)
+        return output
+    else:
+        return None
+
